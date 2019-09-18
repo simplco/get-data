dcf8fa9744e9b7e6a446832800980b4690c1785d
erickdochoa <ochoa.erick.d@gmail.com>
2019-09-18 13:04:03 -0700

    testing Update cloud fn see cloud.go:78, updated .env.yaml with utility api token

--- cloud/cloud.go
+++ cloud/cloud.go
@@ -4,7 +4,9 @@ package cloud
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -48,7 +50,10 @@ type Usage struct {
 }
 
 var (
-	db     *sql.DB
+	db *sql.DB
+
+	token = os.Getenv("UPAI_TOKEN")
+
 	dbUser = os.Getenv("PG_USERNAME")
 	dbPass = os.Getenv("PG_PASSWORD")
 	dbIN   = os.Getenv("PG_INSTANCE_NAME")
@@ -67,6 +72,36 @@ func init() {
 	// Only allow 1 connection to the database to avoid overloading it.
 	db.SetMaxIdleConns(1)
 	db.SetMaxOpenConns(1)
+}
+
+// Update updates db with latest energy data
+func Update(w http.ResponseWriter, req *http.Request) {
+	// var result string
+	// var err error
+
+	now := time.Now()
+
+	start := now.AddDate(0, 0, -3)
+	start.Format("2006-01-02")
+	end := now.AddDate(0, 0, -1)
+	end.Format("2006-01-02")
+
+	u := User{
+		UID: "139376",
+	}
+
+	readings := getLatestReadingsDay(u.UID, start.String(), end.String(), token)
+	fmt.Fprintln(w, readings)
+
+	// _, err = db.Exec("UPDATE users SET ( uid , meter , email , utility , tariff , latestts , wkts , mots , yescons , wkcons , mocons , yescost , wkcost , mocost) = ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) WHERE uid = $1", d.UID, d.Meterid, d.Useremail, d.Utility, d.ServiceTariff, d.LastReading, d.WeekStart, d.MonthStart, d.Yesterday, d.ThisWeek, d.ThisMonth, d.CostYesterday, d.CostThisWeek, d.CostThisMonth)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	result = "damn, insert failed"
+	// 	return result
+	// }
+
+	// result = "db update success"
+	// return result
 }
 
 // Read reads from db
@@ -93,4 +128,54 @@ func Read(w http.ResponseWriter, req *http.Request) {
 	}
 
 	fmt.Fprintf(w, "user: %v", u)
+}
+
+func getLatestReadingsDay(uid string, start string, end string, token string) []interface{} {
+	var url = "https://utilityapi.com/api/v2/intervals?authorizations=" + uid + "&start=" + start + "&end=" + end
+
+	log.Printf("fetching latest day of intervals for meter %v ...\t", uid)
+
+	intervalRes := makeRequest(url, "GET", token)
+	intervals := intervalRes["intervals"].([]interface{})
+	list := intervals[0].(map[string]interface{})
+	readings := list["readings"].([]interface{})
+
+	log.Print("ok\n\n")
+	return readings
+}
+
+func makeRequest(url string, method string, token string) map[string]interface{} {
+	// fmt.Println("\t making ", method, " request to:")
+	// fmt.Println("\t", url)
+
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	req.Header.Add("Authorization", ("Bearer " + token))
+	req.Header.Add("Content-Type", "application/json")
+
+	client := &http.Client{}
+	res, reserr := client.Do(req)
+	if reserr != nil {
+		fmt.Println("res err")
+		log.Fatalln(reserr)
+	}
+	defer res.Body.Close()
+
+	body, bodyerr := ioutil.ReadAll(res.Body)
+	if bodyerr != nil {
+		fmt.Println("body err")
+		log.Fatalln(bodyerr)
+	}
+
+	var data map[string]interface{}
+	dataerr := json.Unmarshal(body, &data)
+	if dataerr != nil {
+		fmt.Println("user err")
+		log.Fatalln(dataerr)
+	}
+
+	return data
 }
